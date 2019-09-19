8986f2f0d742231a3c92ef48395cefc7f814b1fe
wangLei <wangleivic@126.com>
2019-09-19 16:49:37 +0800

    base中增加ajax错误数据返回公共方法

--- routers/router.go
+++ routers/router.go
@@ -8,13 +8,13 @@ import (
 
 func init() {
 	// 账号
-	beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.BaseController{})
 	beego.Router("/account/login", &controllers.AccountController{}, "post:Login")
 	beego.Router("/account/logout", &controllers.AccountController{}, "get:Logout")
 
 	// 脚本管理
 	beego.Router("/web/script/index", &controllers.ScriptController{}, "get:Index")
-	beego.Router("/web/script/list", &controllers.ScriptController{}, "get:List")
+	beego.Router("/web/script/list", &controllers.ScriptController{}, "post:List")
 
 	// API
 	beego.Router("/api/repportInfo/distribute", &controllers.ReportInfoController{}, "*:Distribute")
